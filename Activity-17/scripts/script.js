accf0f977d60f627d4fe688c9b6010881eecb16d
Laksamon25 <Laksamon25@go.byuh.edu>
2025-06-16 09:08:38 -1000

    1

--- Activity-17/scripts/script.js
+++ Activity-17/scripts/script.js
@@ -4,20 +4,24 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
+    var phoneNumber = document.getElementById('phoneNumber').value;
 
     var usernameValid = /^[a-zA-Z0-9]{5,}$/.test(username); // Username should be at least 5 characters long and contain only letters and numbers
     var emailValid = /^[^@]+@\w+(\.\w+)+\w$/.test(email); // Simple email pattern check
     var passwordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password); // Password should be at least 8 characters long, contain numbers and both lowercase and uppercase letters
+    var phoneNumberValid = /^\+?[0-9]{10,15}$/.test(phoneNumber); //Phone number should have 10 numbers //
 
     document.getElementById('usernameFeedback').style.display = usernameValid ? 'none' : 'block';
     document.getElementById('emailFeedback').style.display = emailValid ? 'none' : 'block';
     document.getElementById('passwordFeedback').style.display = passwordValid ? 'none' : 'block';
+    document.getElementById('phoneNumberFeedback').style.display = phoneNumberValid ? 'none' : 'block';
 
     document.getElementById('usernameFeedback').textContent = usernameValid ? '' : 'Username should be at least 5 characters long and contain only letters and numbers.';
     document.getElementById('emailFeedback').textContent = emailValid ? '' : 'Please enter a valid email address.';
     document.getElementById('passwordFeedback').textContent = passwordValid ? '' : 'Password should be at least 8 characters long, contain numbers and both lowercase and uppercase letters.';
+    document.getElementById('phoneNumberFeedback').textContent = phoneNumberValid ? '' : 'Phone Number should be at least 10 numbers';
 
-    var formValid = usernameValid && emailValid && passwordValid;
+    var formValid = usernameValid && emailValid && passwordValid && phoneNumberValid;
 
     if (formValid) {
         document.getElementById('registrationFeedback').textContent = 'Your user registration was accepted!';
